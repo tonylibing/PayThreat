947047e227546a0172122eaa6226f4f4584a4f32
vandeppce <sdtczyj@163.com>
2018-05-30 18:43:13 +0800

    svm

--- Ensemble/EnsembleMain.py
+++ Ensemble/EnsembleMain.py
@@ -5,6 +5,7 @@ from EnsembleModel import EnsembleModel
 from GbdtBaseModel import GbdtModel
 from DenseBaseModel import DenseModel
 from LstmBaseModel import LSTMModel
+from SVMBaseModel import SVMModel
 
 def EnsembleMain(models, model_args, samples=994731, groups=None, processes=24,  na_value='mean',  hot=False):
 
@@ -20,42 +21,56 @@ def EnsembleMain(models, model_args, samples=994731, groups=None, processes=24, 
     paythreat.predict(real_id, real_features)
     print('predict done.')
 
+# models = [SVMModel]
 models = []
+arg = []
 
 # GbdtModel args : normalize methods, feature methods, (model_index, appended in the EnsembleModel)
 gbdt_args = ['standard', 'select']
 gbdt_test_args = ['standard', 'select']
 
+svm_args = ['standard', 'select']
+svm_test_args = ['standard', 'select']
+
 # DenseModel args: normalize methods, feature methods, layers, layer units, batch_size, epochs, steps
 dense_args = ['standard', None, 2, (512, 256), 500, 100, 20000]
 dense_test_args = ['standard', None, 2, (512, 256), 500, 1, 20]
 
-
 # LstmModel args: normalize methods, features methods, time_steps, n_inputs, n_hiddens,keep_prob, n_layers, batch_size, epochs, steps
 lstm_args = ['standard', None, 12, 25, 128, 0.8, 2, 500, 100, 20000]
-lstm_test_args = ('standard', None, 12, 25, 128, 0.8, 2, 500, 1, 20)
+lstm_test_args = ['standard', None, 12, 25, 128, 0.8, 2, 500, 1, 20]
+
+for i in range(4):
+    if i < 1:
+       models.append(LSTMModel)
+       arg.append(lstm_args)
+    if i < 2:
+        models.append(DenseModel)
+        arg.append(dense_args)
+
+    elif i < 3:
+        models.append(SVMModel)
+        arg.append(svm_args)
+
+    else:
+        models.append(GbdtModel)
+        arg.append(gbdt_args)
+
 model_args = {
-    'args':[
-        gbdt_args , # gbdt args
-        dense_args,
-        lstm_args
-    ],
+    'args': arg,
     'weights': None
 }
 
 model_test_args = {
-    'args':[
-        lstm_test_args
+'args':[
+        # gbdt_test_args , # gbdt args
+        # dense_test_args,
+        # lstm_test_args,
+        # svm_test_args
+        fc_test_args,
     ],
     'weights': None
 }
-
-for i in range(2):
-    models.append(LSTMModel)
-    model_test_args['args'].append(lstm_test_args)
-
-EnsembleTest = partial(EnsembleMain, samples=4000, groups=len(models), processes = 2, models = models, model_args = model_test_args)
+EnsembleTest = partial(EnsembleMain, samples=1000, processes = 2, models = models, model_args = model_test_args)
 Ensemble = partial(EnsembleMain, models=models, model_args=model_args)
-
-if __name__ == '__main__':
-    EnsembleTest()
+EnsembleTest()
---
+++ Ensemble/SVMBaseModel.py
@@ -0,0 +1,26 @@
+from BaseModel import BaseModel
+from sklearn.svm import SVC
+
+class SVMModel(BaseModel):
+    def __init__(self, args):
+        super().__init__()
+        self.normalize, self.fea_deal, self.model_index = args[0], args[1], args[2]
+        self._model = SVC(probability=True)
+        self.fit_done = 0
+
+    @staticmethod
+    def get_name():
+        return 'svm'
+
+    @classmethod
+    def nn_model(clf):
+        return False
+
+    def fit(self, features, labels):
+        _features = self.prepare_fea(features, labels)
+        self._model.fit(_features, labels)
+        self.fit_done = 1
+
+    def predict_prob(self, features):
+        _features = self.prepare_fea(features)
+        return self._model.predict_proba(_features)[:,1]
